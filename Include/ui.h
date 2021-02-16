5c70d30d57a4dee4f4de453862bfa61bc73494f3
MatsBraunerDerGauner <faktorx24@gmail.com>
2021-02-16 09:27:54 +0100

    Linux v0.0.0

--- Include/ui.h
+++ Include/ui.h
@@ -16,14 +16,37 @@
 #define CLEAR system("cls")
 #endif
 
+// check dynamic store
+#define check(x) \
+    if (x == NULL) { \
+        printf("dynamic store faild in line %d", __LINE__); \
+        exit(EXIT_FAILURE); \
+    }
+
+// char for the Curser
+#define curser "⇒"
+
+// menupoints char buffer max
+#define BUFFMAX 50
+
+// struct for page
+typedef struct page_t {
+    int menuPointCount;
+    char *title;
+    char **menuPoints;
+    struct page_t *pre;
+    struct page_t *next;
+} page_t;
+
+extern page_t *pageList;
+
+static page_t *createPage_t(int menuPointCount, ...);
+
 // Keys for the movement in the menu
 extern enum { down = 'n', up = 'r', enter = '\n' } key;
 
-// Draw's the Rechnen menu point
-static void drawRechnen(void);
-
-// Draw's the Formeln menu point
-static void drawFormeln(void);
+// Build's the Page about the page
+static page_t *drawPage(page_t *page, _Bool isMainPage);
 
 // Draw's the Menu when the Programm is Started
 void drawMenu(void);
--- ui.c
+++ ui.c
@@ -1,36 +1,102 @@
 #include"Include/ui.h"
 
-static void drawRechnen(void) {
-    CLEAR;
-    printf("RechnenPage");
-    getch();
+
+page_t *pageList = NULL;
+
+static page_t *createPage_t(int menuPointCount, ...) {
+    va_list ptr_va_list;
+
+    va_start(ptr_va_list, menuPointCount);
+
+    // page list 
+    if (pageList == NULL) {
+        pageList = malloc(sizeof(page_t));
+        check(pageList);
+
+        pageList->menuPointCount = menuPointCount;
+
+        pageList->title = va_arg(ptr_va_list, char *);
+        
+        pageList->menuPoints = calloc(menuPointCount, sizeof(char *));  
+        check(pageList->menuPoints);
+        
+        for (int i = 0; i < menuPointCount; i++) {
+            *(pageList->menuPoints + i) = calloc(BUFFMAX, sizeof(char));
+            check(*(pageList->menuPoints + i));
+            *(pageList->menuPoints + i) = va_arg(ptr_va_list, char *);
+        }
+
+        pageList->pre = NULL;
+        pageList->next = NULL;
+
+        va_end(ptr_va_list);
+
+        return pageList;
+    } else {
+        page_t *ptr, *ptrPre;
+        ptr = pageList;
+
+        do {
+            ptrPre = ptr;
+            ptr = ptr->next;
+        } while (ptr != NULL);
+
+
+        ptr = malloc(sizeof(page_t));
+        check(ptr);
+
+        ptr->menuPointCount = menuPointCount;
+
+        ptr->title = va_arg(ptr_va_list, char *);
+        
+        ptr->menuPoints = calloc(menuPointCount, sizeof(char *));  
+        check(ptr->menuPoints);
+        
+        for (int i = 0; i < menuPointCount; i++) {
+            *(ptr->menuPoints + i) = calloc(BUFFMAX, sizeof(char));
+            check(*(ptr->menuPoints + i));
+            *(ptr->menuPoints + i) = va_arg(ptr_va_list, char *);
+        }
+
+        ptrPre->next = ptr;
+
+        ptr->pre = ptrPre;
+        ptr->next = NULL;
+
+        va_end(ptr_va_list);
+
+        return ptr;
+    }
+
+    return 0;
 }
 
-static void drawPage(int count, ...) {
-    va_list ptr;
-    char *str;
-
-    va_start(ptr, count);
-}
-
-static void drawFormeln() {
+static page_t *drawPage(page_t *page, _Bool isMainPage) {
     // The position of the Curser « ⇒ »
     int curserPos = 0;
-    // Max fo menu points
-    const int CURSER_POS_MAX = 2;
 
     while (1) {
         CLEAR;
-        char *curser = "⇒";
-        printf("\r----------------\n\
-                \r| - Formeln -  |\n\
-                \r----------------\n\
-                \r|%s Neue Formel |\n\
-                \r----------------\n\
-                \r|%s Back        |\n\
-                \r----------------\n",
-                (curserPos == 0) ?curser :" ",
-                (curserPos == 1) ?curser :" ");
+
+        // title
+        printf("\r-------------\n\
+                \r| - %s -       |\n", 
+                page->title);
+
+        // menu points
+        for (int i = 0; i < page->menuPointCount; i++) {
+            printf("\r------------\n\
+                    \r|%s %s       \n",
+                    (curserPos == i) ?curser :" ",
+                    *(page->menuPoints + i));
+        }
+
+        // exit or back
+        printf("\r-----------\n\
+                \r|%s %s    \n\
+                \r-----------\n",
+                (curserPos == page->menuPointCount) \
+                ?curser :" ", (isMainPage) ?"EXIT" :"BACK");
 
         // Key abfrage
         int c;
@@ -40,7 +106,7 @@ static void drawFormeln() {
 
         // Key pressed processing
         if (c == down) {
-            if (curserPos != CURSER_POS_MAX - 1) 
+            if (curserPos != page->menuPointCount) 
                 curserPos++;
             continue; 
         }
@@ -52,62 +118,40 @@ static void drawFormeln() {
         }
 
         if (c == enter) {
-            switch (curserPos) {
-                case 1: return; break;
-                default: break;
+            if (curserPos == page->menuPointCount) {
+                if (isMainPage) {
+                    exit(EXIT_FAILURE);
+                } else { 
+                    page_t *ptr = pageList;
+                    while (ptr != NULL) {
+                        for (int i = 0; i < ptr->menuPointCount; i++) {
+                            if (*(ptr->menuPoints + i) == page->title)
+                                return ptr;
+                        }
+                        ptr = ptr->next;
+                    }
+                }
+            }
+
+            page_t *ptr = pageList;
+            while (ptr != NULL) {
+                if (*(page->menuPoints + curserPos) == ptr->title)
+                    return ptr;
+                ptr = ptr->next;
             }
         }
     }
+        return NULL; 
 }
 
 void drawMenu(void) {
-    // The position of the Curser « ⇒ »
-    int curserPos = 0;
-    // Max fo menu points
-    const int CURSER_POS_MAX = 3;
+    createPage_t(2, "Menu", 
+            "Formeln", "Rechnen");
+    createPage_t(2, "Formeln",
+            "ADD", "Delete");
 
+    page_t *ptr = pageList;
     while (1) {
-        CLEAR;
-        char *curser = "⇒";
-        printf("\r------------\n\
-                \r| - Menu - |\n\
-                \r------------\n\
-                \r|%s Rechnen |\n\
-                \r-------------\n\
-                \r|%s Formeln |\n\
-                \r------------\n\
-                \r|%s Exit    |\n\
-                \r------------\n",
-                (curserPos == 0) ?curser :" ",
-                (curserPos == 1) ?curser :" ",
-                (curserPos == 2) ?curser :" ");
-
-        // Key abfrage
-        int c;
-        do {
-            c = getch();
-        } while (c != down && c != up && c != enter);
-        
-        // Key pressed processing
-        if (c == down) {
-            if (curserPos != CURSER_POS_MAX - 1) 
-                curserPos++;
-            continue; 
-        }
-
-        if (c == up) { 
-            if (curserPos != 0) 
-                curserPos--; 
-            continue;
-        }
-
-        if (c == enter) {
-            switch (curserPos) {
-                case 0: drawRechnen(); break;
-                case 1: drawFormeln(); break;
-                case 2: exit(EXIT_FAILURE); break;
-                default: break;
-            }
-        }
+        ptr = drawPage(ptr, (ptr->title == pageList->title) ?1 :0);
     }
 }
